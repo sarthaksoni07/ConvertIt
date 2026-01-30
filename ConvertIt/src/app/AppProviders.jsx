ea70d82e1b61a91984c79c7a21f0be0bdee6de76
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-01-30 09:47:44 +0530

    fix(bug)

--- ConvertIt/src/app/AppProviders.jsx
+++
@@ -1,9 +0,0 @@
-import { AppProvider } from "../context/AppContext";
-
-export default function AppProviders({ children }) {
-  return (
-    <AppProvider>
-      {children}
-    </AppProvider>
-  );
-}
