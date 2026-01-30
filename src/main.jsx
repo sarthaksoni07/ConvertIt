25d4f14c138cb53b30fef7c0055d8e211ce16e6c
sarthaksoni07 <sarthaksoni07@gmail.com>
2026-01-30 12:23:57 +0530

    fix(bug):fixed filename referenceing bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
-import AppProviders from "./app/AppProviders";
+import App from "./App/App";
+import AppProviders from "./App/AppProviders";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
